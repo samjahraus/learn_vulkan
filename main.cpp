b2d1e5b55bc414a20bdd67660803d11c0f584559
sjjahraus <sam.jjahraus@gmail.com>
2021-05-22 20:41:13 -0600

    Added basic glfw support

--- main.cpp
+++ main.cpp
@@ -1,7 +1,36 @@
 #include <iostream>
+#include <GLFW/glfw3.h>
 
 int main() {
-    std::cout << "Hello World!" << "\n";
+    
+    GLFWwindow* window;
+
+    if( !glfwInit() )
+    {
+        fprintf( stderr, "Failed to initialize GLFW\n" );
+        exit( EXIT_FAILURE );
+    }
+
+    glfwWindowHint(GLFW_DEPTH_BITS, 16);
+    glfwWindowHint(GLFW_TRANSPARENT_FRAMEBUFFER, GLFW_TRUE);
+
+    window = glfwCreateWindow( 300, 300, "Gears", NULL, NULL );
+    if (!window)
+    {
+        fprintf( stderr, "Failed to open GLFW window\n" );
+        glfwTerminate();
+        exit( EXIT_FAILURE );
+    }
+
+        // Main loop
+    while( !glfwWindowShouldClose(window) )
+    {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // Terminate GLFW
+    glfwTerminate();
 
     return 0;
 }
