138ec72c9559fc563eadeb5e6676f291c5de007b
sjjahraus <sam.jjahraus@gmail.com>
2021-05-26 14:09:11 -0600

    Finished Vulkan validation layer support.

--- src/main.cpp
+++ src/main.cpp
@@ -22,6 +22,30 @@ const std::vector<const char*> validation_layers = {
     const bool enable_validation_layers = true;
 #endif 
 
+VkResult create_debug_utils_messenger_ext(
+    VkInstance instance,
+    const VkDebugUtilsMessengerCreateInfoEXT* p_create_info,
+    const VkAllocationCallbacks* p_allocator,
+    VkDebugUtilsMessengerEXT* p_debug_messenger) {
+
+    auto func = (PFN_vkCreateDebugUtilsMessengerEXT) vkGetInstanceProcAddr(instance, "vkCreateDebugUtilsMessengerEXT");
+    if (func != nullptr) {
+        return func(instance, p_create_info, p_allocator, p_debug_messenger);
+    } else {
+        return VK_ERROR_EXTENSION_NOT_PRESENT;
+    }
+}
+
+void destroy_debug_utils_messenger_ext(VkInstance instance,
+    VkDebugUtilsMessengerEXT debug_messenger,
+    const VkAllocationCallbacks* p_allocator) {
+
+        auto func = (PFN_vkDestroyDebugUtilsMessengerEXT) vkGetInstanceProcAddr(instance, "vkDestroyDebugUtilsMessengerEXT");
+        if (func != nullptr) {
+            func(instance, debug_messenger, p_allocator);
+        }
+}
+
 class Application {
 public:
     void run() {
@@ -58,6 +82,10 @@ private:
     }
 
     void cleanup() {
+        if (enable_validation_layers) {
+            destroy_debug_utils_messenger_ext(instance, debug_messenger, nullptr);
+        }
+
         vkDestroyInstance(instance, nullptr);
 
         glfwDestroyWindow(window);
@@ -93,28 +121,46 @@ private:
         create_info.enabledExtensionCount = static_cast<uint32_t>(extensions.size());
         create_info.ppEnabledExtensionNames = extensions.data();
 
-        create_info.enabledLayerCount = 0;
+        VkDebugUtilsMessengerCreateInfoEXT debug_create_info;
+        if (enable_validation_layers) {
+            create_info.enabledLayerCount = static_cast<uint32_t>(validation_layers.size());
+            create_info.ppEnabledLayerNames = validation_layers.data();
 
-        ///Print Extensions
-        /*
-        uint32_t extension_count = 0;
-        vkEnumerateInstanceExtensionProperties(nullptr, &extension_count, nullptr);
-        std::vector<VkExtensionProperties> extensions(extension_count);
-        vkEnumerateInstanceExtensionProperties(nullptr, &extension_count, extensions.dcd ata());
+            populate_debug_messenger_create_info(debug_create_info);
+            create_info.pNext = (VkDebugUtilsMessengerCreateInfoEXT*) &debug_create_info;
+        } else {
+            create_info.enabledLayerCount = 0;
+            create_info.pNext = nullptr;
+        }
 
-        std::cout << "available extensions:\n";
-
-        for (const auto& extension : extensions) {
-            std::cout << '\t' << extension.extensionName << "\n";
-        }*/
 
         if (vkCreateInstance(&create_info, nullptr, &instance) != VK_SUCCESS) {
             throw::std::runtime_error("failed to create instance!");
         }
     }
 
+    void populate_debug_messenger_create_info(VkDebugUtilsMessengerCreateInfoEXT& create_info) {
+        create_info = {};
+        create_info.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
+        create_info.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT | 
+                                        VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT |
+                                        VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
+        create_info.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT |
+                                    VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT |
+                                    VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
+        create_info.pfnUserCallback = debug_callback;
+        create_info.pUserData = nullptr;
+    }
+
     void setup_debug_messenger() {
         if (!enable_validation_layers) return;
+
+        VkDebugUtilsMessengerCreateInfoEXT create_info;
+        populate_debug_messenger_create_info(create_info);
+
+        if (create_debug_utils_messenger_ext(instance, &create_info, nullptr, &debug_messenger) != VK_SUCCESS) {
+            throw std::runtime_error("failed to setup debug messenger!");
+        }
     }
 
     std::vector<const char*> get_required_extensions() {
