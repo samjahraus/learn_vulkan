9208521af9723ba2df587c2864bd362cc8d8618c
sjjahraus <sam.jjahraus@gmail.com>
2021-06-07 14:42:55 -0600

    Updated CMake build system more.

---
+++ windows_build.bat
@@ -0,0 +1,17 @@
+rmdir build
+
+mkdir build
+cd build
+mkdir windows
+
+cd ..
+
+cmake -S . -B ./build/windows -G"Visual Studio 16 2019" -A"x64"
+cd build
+cd windows
+
+msbuild learn_vulkan.sln /t:Rebuild /p:Configuration=Debug
+
+cd ..
+cd ..
+pause
