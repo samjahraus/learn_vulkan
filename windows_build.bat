1ea2685a197d47a336a089c3a1485f625adcb98f
sjjahraus <sam.jjahraus@gmail.com>
2021-06-14 18:32:43 -0600

    Finally a triangle!

--- windows_build.bat
+++ windows_build.bat
@@ -10,7 +10,7 @@ cmake -S . -B ./build/windows -G"Visual Studio 16 2019" -A"x64"
 cd build
 cd windows
 
-msbuild learn_vulkan.sln /t:Rebuild /p:Configuration=Debug
+msbuild learn_vulkan.sln /t:Rebuild /p:Configuration=Release
 cd ..
 cd ..
 pause
